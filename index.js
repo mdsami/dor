e6ab46c57592dcc4fad50895bdcdf33777105aa9
Masum Billah Sanjid <54928529+sanjidbillah@users.noreply.github.com>
2022-06-19 15:01:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,40 @@
-const PORT = 5000;
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 const app = express();
 
-// async function cryptopriceScraper() {
-//   const url = "http://www.dam.gov.bd/";
-//   const coinArray = [];
-//   await axios(url).then((response) => {
-//     const html_data = response.data;
-//     const $ = cheerio.load(html_data);
+async function latestPriceScrapping() {
+  const url = "http://dam.gov.bd/?L=B";
+  const priceArray = [];
+  await axios(url).then((response) => {
+    const html_data = response.data;
+    const $ = cheerio.load(html_data);
+    $('#wrapper > div > #bcontainer > #bbody > div.mymarquee > #marqueeborder > #marqueecontent > span.stockbox ').each((index, elem) => {
 
-    // const selectedElem =
-    //   ".mymarquee  > div > div.mymarquee > div.marqueecontent > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
-    // const keys = [
-    //   "No.",
-    //   "Coin",
-    //   "Price",
-    //   "24h",
-    //   "7d",
-    //   "Marketcap",
-    //   "Volume",
-    //   "CirculatingSupply",
-    // ];
+      $(elem).children().each((childID, childElem) => {
+
+        priceArray.push($(childElem).parent().next().text());
+      })
+    })
 
 
-    async function cryptopriceScraper() {
-        const url = "https://coinmarketcap.com/";
-        const coinArray = [];
-        await axios(url).then((response) => {
-          const html_data = response.data;
-          const $ = cheerio.load(html_data);
-      
-    const selectedElem =
-      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
-    const keys = [
-      "No.",
-      "Coin",
-      "Price",
-      "24h",
-      "7d",
-      "Marketcap",
-      "Volume",
-      "CirculatingSupply",
-    ];
-
-    $(selectedElem).each((parentIndex, parentElem) => {
-      let keyIndex = 0;
-      const coinDetails = {};
-      if (parentIndex <= 9) {
-        $(parentElem)
-          .children()
-          .each((childId, childElem) => {
-            const value = $(childElem).text();
-            if (value) {
-              coinDetails[keys[keyIndex]] = value;
-              keyIndex++;
-            }
-          });
-        coinArray.push(coinDetails);
-      }
-    });
   });
-  return coinArray;
+  return priceArray;
 }
 
-app.get("/api/crypto", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const crypto = await cryptopriceScraper();
-    return res.status(200).json({
-      result: crypto,
-    });
+    const pricingLIst = await latestPriceScrapping();
+    return res.status(200).json(pricingLIst);
   } catch (err) {
     return res.status(500).json({
       err: err.toString(),
     });
   }
 });
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`The server is active and running on port ${PORT}`)
 );
 
-// Api url GET -> http://localhost:5000/api/crypto
